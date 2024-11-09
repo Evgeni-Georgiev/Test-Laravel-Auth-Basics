def9c1a0787683645a935bca8847880480ccd1ed
Evgeni Georgiev <evgeni.georgiev@donatix.net>
2024-11-09 19:32:40 +0200

    Solution for Test-Laravel-Auth-Basics

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -14,6 +15,17 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
+        $user = auth()->user();
+        $data = $request->validated();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($data['password']);
+        }
+
+        $user->save();
+
         // Also, update the password if it is set
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
